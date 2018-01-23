0fed9720aea969c58821e9940148e05d86366bb1
Lalit Sahni <lalit.sahni@gmail.com>
2018-01-23 14:16:42 +0530

    list1

---
+++ lalitpython/DS/list1.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+x=5
+x
+l=[1,2,"a"]
+l
+type(l)
+t=(1,2,"a")
+type(t)
+d={"a":1,"b":2,"d":1}
+f=frozenset({3,4,5})
+type(f)
+b="nice"
+b
+a='ls'
+a
+my_list
